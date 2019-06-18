c7014b3431725763dca572682dbfb6733b54d8a5
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-06-18 00:02:47 -0500

    Rehice una parte del archivo html 1, casi está completo el CSS del html 1 y el cifrado de la palabra ya se muestra

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,25 @@
 window.cipher = {
-  
+     encode : (offset, string) => {
+
+        string = string.toUpperCase();
+        offset = parseInt(offset);
+        let desplazamiento = "";
+        let encripted = "";
+
+        for (let i = 0; i < string.length; i ++) {
+            
+            let ascii = string.charCodeAt(i); //nos da su posicion ascii
+            desplazamiento =  (ascii-65+offset)%26+65; // nos recorre el offset 
+            encripted = encripted+String.fromCharCode(desplazamiento);
+            document.getElementById("encriptedkeyword").innerHTML= encripted;
+
+            
+            
+        }
+
+       
+        console.log(encripted);
+
+    }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,53 +1,20 @@
-//de APP1 a APP2
-//SING UP quiero que baje no necesito js
-//de APP1 a APP5
-// CODIGO DE ACCESO 
-
-const mostrarCodigoDeAcceso = () => {
-    let aparecerCodigoDeAcceso = document.getElementById("codigoDeAcceso");
-    aparecerCodigoDeAcceso.style.display = "block";
-    return aparecerCodigoDeAcceso;
-}
-
-const enlaceCodigoDeAcceso = document.getElementById("enlaceCodigoDeAcceso");
-    enlaceCodigoDeAcceso.addEventListener("click",mostrarCodigoDeAcceso);
 
 
-
-// de APP2 a APP3
-
-const mostrarCuentaCreada = () => {         
-    let aparecerCuentaCreada = document.getElementById("cuentaCreada");    
-    aparecerCuentaCreada.style.display = "block";
-
-
-
-
-
+const mostrarCodigoCreado = () => {         
+    let aparecerCodigoCreado = document.getElementById("codigoCreado");    
+    aparecerCodigoCreado.style.display = "block";
+    let desaparecerFormulario = document.getElementById("crearCuenta");
+    desaparecerFormulario.style.display = "none";
+    
     let string = document.getElementById("keyword").value;
     let offset = document.getElementById("offset").value;
-    /*window.cipher.encode(offset,string);//invoca al metodo que se ecnuntra en window.cipher , hay que escribir la "ruta" hacua */
+  
+    window.cipher.encode(offset, string) //llamas a la funcion en cipher
+
     
-    /*bienvenido tu codigo cifrado es, cómo lo conecto sin esto?*/ 
-        let tamano = string.length;      /*tamaño de la palabra*/
-        let numOf = parseInt(offset);   /*numero*/
-        let ascii;                        /*pasar a ascii*/  
-        let formula;                      
-        let cifrado = "";
-
-
-        for (let i=0; i<tamano; i++){
-
-          ascii = string.charCodeAt(i);
-          formula = (ascii-65+numOf)%26+65;
-          cifrado = cifrado+String.fromCharCode(formula); 
-
-          console.log(cifrado);
-
-        }
-
-       
-}
+    }
 
 const enlaceCuentaCreada = document.getElementById("enlaceBienvenida");
-    enlaceCuentaCreada.addEventListener("click",mostrarCuentaCreada);
+    enlaceCuentaCreada.addEventListener("click",mostrarCodigoCreado);
+
+    
