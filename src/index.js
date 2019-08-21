3aa15d12f421dcc2313e3e47a508a0289fb81913
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-08-21 17:14:39 -0500

    actualizando con funciones puras y test aprobados

--- src/index.js
+++ src/index.js
@@ -1,39 +1,28 @@
   
 const mostrarCodigoCreado = () => {         
-    
-    
+
     let aparecerCodigoCreado = document.getElementById("codigoCreado");    
     aparecerCodigoCreado.style.display = "block";
     let desaparecerFormulario = document.getElementById("crearCuenta");
     desaparecerFormulario.style.display = "none"; 
     let string = document.getElementById("keyword1").value;
     let offset = document.getElementById("offset1").value;
-    
-
-    
     window.cipher.encode(offset, string);   
-
-
-    
-
+    document.getElementById("encryptedkeyword").innerHTML = window.cipher.encode(offset, string);
 }
 
-
-
-
 const enlaceCuentaCreada = document.getElementById("enlaceBienvenida");
-    enlaceCuentaCreada.addEventListener("click",mostrarCodigoCreado);
-
+    enlaceCuentaCreada.addEventListener("click",mostrarCodigoCreado);   
 
 const mostrarPuerta = () => {
     let aparecerPuerta =  document.getElementById("puerta");
     aparecerPuerta.style.display="block";
 }
-const enlacePuerta = document.getElementById("enlacePuerta");
-    enlacePuerta.addEventListener("click",mostrarPuerta);
 
-   
-const puedesEntrar = () => {
+const enlacePuerta = document.getElementById("enlacePuerta");
+    enlacePuerta.addEventListener("click",mostrarPuerta);  
+
+    const puedesEntrar = () => {
 
     let name1 = document.getElementById("name1").value;
     let name2 = document.getElementById("name2").value;
@@ -41,42 +30,22 @@ const puedesEntrar = () => {
     let palabraEncriptada = document.getElementById("encryptedkeyword").textContent;
     let desaparecerPuerta = document.getElementById("puerta");
     let offset2 = document.getElementById("offset1").value;
-    
-       
-
     if (codigo === "") {
         alert("Debes llenar los campos");
-
-    }    else if (name1 === name2 && codigo === palabraEncriptada) {
- 
-        
+    }   else if (name1 === name2 && codigo === palabraEncriptada) {        
         let codigoCorrecto = document.getElementById("correcto");
         codigoCorrecto.style.display="block";        
-        desaparecerPuerta.style.display="none";     
-  
-
-        }  else {
-
+        desaparecerPuerta.style.display="none";    
+    }  else {
         let codigoIncorrecto =document.getElementById("incorrecto");
         codigoIncorrecto.style.display="block";            
         desaparecerPuerta.style.display="none"; 
-        }
-
-        
-     
+        }     
     window.cipher.decode(offset2, palabraEncriptada);
-
-
-    
-}
-      
-
-
-   
+    document.getElementById("decryptedword").innerHTML=window.cipher.decode(offset2, palabraEncriptada);
+}   
 const enlaceCorrecto  = document.getElementById("enlaceCorrecto");
 enlaceCorrecto.addEventListener("click",puedesEntrar);
-
-
 let inicio = document.getElementById("inicio");
 let btn = document.getElementById("icono2");
 btn.addEventListener("click", () => {
@@ -86,7 +55,6 @@ btn.addEventListener("click", () => {
         behavior: "smooth"
     })
 })    
-
 const volverAIntentar = () => {
     let aparecerDoor1 =  document.getElementById("puerta");
     aparecerDoor1.style.display="block";
